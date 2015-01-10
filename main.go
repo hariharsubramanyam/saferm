b41796631b868657ccfba336cccf3404ef3dcc6a
Harihar Subramanyam <hahasubbu@gmail.com>
2015-01-10 11:25:59 -0500

    MVP

--- main.go
+++ main.go
@@ -8,10 +8,48 @@ Moves file to .safetrash in home directory.
 package main
 
 import (
-	"fmt"
 	"os"
+	"path"
+	"runtime"
 )
 
+const TRASHNAME = ".safetrash"
+
 func main() {
-	fmt.Println(os.Args[1])
+
+	workingDirectory, err := os.Getwd()
+	if err == nil {
+		fileName := os.Args[1]
+		moveFileToTrash(fileName, workingDirectory)
+	}
+}
+
+func moveFileToTrash(fileName string, containingDirectory string) {
+	originalPath := path.Join(containingDirectory, fileName)
+	if pathExists(originalPath) {
+		// Create ~/.safetrash if it doesn't exist.
+		trashPath := path.Join(homeDirectoryPath(), TRASHNAME)
+		if !pathExists(trashPath) {
+			os.Mkdir(trashPath, os.ModePerm)
+		}
+
+		newPath := path.Join(trashPath, fileName)
+		os.Rename(originalPath, newPath)
+	}
+}
+
+func pathExists(path string) bool {
+	_, err := os.Stat(path)
+	return err == nil
+}
+
+func homeDirectoryPath() string {
+	if runtime.GOOS == "windows" {
+		home := os.Getenv("HOMEDRIVE") + os.Getenv("HOMEPATH")
+		if home == "" {
+			home = os.Getenv("USERPROFILE")
+		}
+		return home
+	}
+	return os.Getenv("HOME")
 }
