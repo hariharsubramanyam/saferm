d21317c39ac110f656a769aa46ec4eb8aa212af6
Harihar Subramanyam <hahasubbu@gmail.com>
2015-01-10 23:12:13 -0500

    Commenting file utils

--- trashman/file_helpers.go
+++ trashman/file_helpers.go
@@ -5,13 +5,15 @@ in the safe trash.
 package trashman
 
 import (
-	"os"
-	"path"
-	"runtime"
+	"os"      // For file ops.
+	"path"    // For joining paths.
+	"runtime" // For getting OS version (to find HOME directory).
 )
 
+// trashfile is the directory where "deleted" files/directories will be moved to.
 const trashfile = ".safetrash"
 
+// HomeDirectoryPath is the path to the user's HOME directory (or equivalent, if not UNIX).
 func HomeDirectoryPath() string {
 	if runtime.GOOS == "windows" {
 		home := os.Getenv("HOMEDRIVE") + os.Getenv("HOMEPATH")
@@ -23,15 +25,19 @@ func HomeDirectoryPath() string {
 	return os.Getenv("HOME")
 }
 
+// TrashPath is the path to the trash where "deleted" files go to.
 func TrashPath() string {
 	return path.Join(HomeDirectoryPath(), trashfile)
 }
 
+// PathExists determines whether a given path (to either a file or directory) exists.
 func PathExists(path string) bool {
 	_, err := os.Stat(path)
 	return err == nil
 }
 
+// MoveFileToTrash moves the given fileName (which is inside the given containingDirectory) to the
+// safe trash.
 func MoveFileToTrash(containingDirectory string, fileName string) {
 	originalPath := path.Join(containingDirectory, fileName)
 	if !PathExists(originalPath) {
