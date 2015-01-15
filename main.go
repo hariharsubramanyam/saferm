5ea0434977c5cdcafb7b7c40291878a12827942f
Harihar Subramanyam <hahasubbu@gmail.com>
2015-01-14 21:56:59 -0500

    Allow changing trash size in command line arg

--- main.go
+++ main.go
@@ -10,18 +10,31 @@ package main
 //TODO(harihar): Update the package comment after the MVP.
 
 import (
+	"flag"
+	"fmt"
 	"github.com/hariharsubramanyam/saferm/trash"
 	"os"
 )
 
 func main() {
+	trashSize := flag.Int("trashsize", -1, "Set the trash size in MB")
+
+	flag.Parse()
 	// Create a Trash object to handle the user's action.
 	userTrash := trash.NewTrash()
+
+	newTrashSize := 1024 * 1024 * *trashSize
+	if newTrashSize >= trash.MinTrashSize && newTrashSize <= trash.MaxTrashSize {
+		fmt.Println("Changing trash size")
+		userTrash.TrashSize = newTrashSize
+		userTrash.Save()
+	}
 
 	workingDirectory, err := os.Getwd()
 	if err == nil {
 		// Delet the file.
-		fileName := os.Args[1]
+		fileName := flag.Arg(0)
+		fmt.Println(fileName)
 		userTrash.DeleteFile(workingDirectory, fileName)
 	}
 
